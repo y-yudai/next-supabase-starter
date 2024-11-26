61fcb9618bd26fd94c2d044a30fcb5fc40a5672c
山本 雄大 <yudai.yamamoto@MAC026.local>
2024-11-26 10:32:53 +0900

    add : ui templates

---
+++ src/app/m/profile/page.tsx
@@ -0,0 +1,16 @@
+import type { Metadata } from 'next'
+import PageClient from './page.client'
+
+export function generateMetadata(): Metadata {
+  return {
+    title:``
+  }
+}
+
+export default function Profile() {
+  return (
+    <div>
+      <PageClient />
+    </div>
+  );
+}
