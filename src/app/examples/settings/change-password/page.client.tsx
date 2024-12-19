701ec30b85753168e1eb4f35d4c4f640411fb98d
山本 雄大 <yudai.yamamoto@MAC026.local>
2024-12-19 15:15:10 +0900

    add: changepassword

--- src/app/examples/settings/change-password/page.client.tsx
+++ src/app/examples/settings/change-password/page.client.tsx
@@ -1,14 +1,33 @@
 'use client'
 
 import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import useAuthStore from '@/store/auth'
+import useUserStore from '@/store/user-account'
 import { Button } from "@/components/ui/button"
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
 export default function PageClient() {
+  const router = useRouter()
+  const { updatePassword, logout } = useAuthStore()
+  const { clearUserAccount } = useUserStore()
   const [newPassword, setNewPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
-  const changePassword = async () => {}
+  const changePassword = async () => {
+    if (newPassword !== confirmPassword) {
+      alert('Passwords do not match')
+      return
+    }
+    try {
+      await updatePassword(newPassword)
+      await clearUserAccount()
+      await logout()
+      router.push('/examples/signin');
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   return (
     <main className="mx-auto flex max-w-screen-md justify-center">
